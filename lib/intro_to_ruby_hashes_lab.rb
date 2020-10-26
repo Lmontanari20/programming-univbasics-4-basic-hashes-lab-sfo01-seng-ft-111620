e637274c2a1f1fc1eade3eeb5ad031b7e7c4833d
Lucas Montanari <lmonty2011@gmail.com>
2020-10-26 18:07:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,27 @@
 def new_hash
   # return an empty hash
-  hash.Hash.new
+  hash = Hash.new
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  h = {
+    name: "lucas"
+  }
+  h
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  h = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  h = {
+    id: number
+  }
 end
